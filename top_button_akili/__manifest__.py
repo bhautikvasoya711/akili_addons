680315a1b656b22fe4c41871354f408b726db10e
Bhautik Vasoya <bavasoya@gmail.com>
2018-10-26 18:04:00 +0530

    [ADD]Test

--- top_button_akili/__manifest__.py
+++ top_button_akili/__manifest__.py
@@ -1,20 +1,23 @@
   # -*- encoding: utf-8 -*-
  
 {
-    'name': 'Scroll to top button.',
+    'name': 'Scroll To Top Button.',
     'summary': 'Scroll to top button.',
-    'description': 'Scroll to top button.',
+    'description': 'Scroll to top button on single click.',
     'version': '1.0',
     'category': 'Website',
     'depends': ['website'],
-    'author': "Akil system",
+    'company': "Akil Systems Pvt. Ltd.",
+    'author': "Akil System Pvt. Ltd.",
     'website': "http://www.akilisystems.in/",
     'data': [
         'view/assets_frontend.xml',
     ],
 
     'images': [
-        'static/description/icon.png',],
+        'static/description/image.jpg',
+
+        ],
     
     'application': False,
     'installable': True,
--- top_button_akili/static/src/js/scroll_totop.js
+++ top_button_akili/static/src/js/scroll_totop.js
@@ -1,15 +1,20 @@
+odoo.define('webtop_button_akili.scroll_totop', function (require) {
+'use strict';
 
+	var core = require('web.core');
+	var _t = core._t;
 
-window.onscroll = function() {scrollFunction()};
+	window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+	function scrollFunction() {
+	    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	        document.getElementById("myBtn").style.display = "block";
+	    } else {
+	        document.getElementById("myBtn").style.display = "none";
+	    }
+	}
+	function topFunction() {
+	    document.body.scrollTop = 0;
+	    document.documentElement.scrollTop = 0;
+	}
+});
