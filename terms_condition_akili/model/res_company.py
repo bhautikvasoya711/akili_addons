ed84b43f67ed2241f135c977c5d149b746a127ab
Bhautik Vasoya <bavasoya@gmail.com>
2018-10-27 12:10:50 +0530

    Delete Module

--- terms_condition_akili/model/res_company.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import  fields, models
-
-
-class Company(models.Model):
-    _inherit = "res.company"
-
-    enable_feature = fields.Boolean(string='Enable Feature',)
-    website_description = fields.Html(string='Website term description',)
